47110cf33a5c016bf0461d4e83ad781195987ac5
unknown <majiar16@gmail.com>
2022-09-06 11:27:51 -0500

    update sidebar items

--- src/app/components/sidebar-item/sidebar-item.component.ts
+++ src/app/components/sidebar-item/sidebar-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-sidebar-item',
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sidebar-item.component.css']
 })
 export class SidebarItemComponent implements OnInit {
-
+  @Input() title: string = '';
+  @Input() classIcono: string = '';
   constructor() { }
 
   ngOnInit(): void {
