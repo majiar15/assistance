6cc0993f9911c640134af7f42f4c50773ace18c2
Daniel Salinas <danielsalinassalinas09@gmail.com>
2022-11-29 16:13:24 -0500

    filtrar asistencias por fecha

--- src/app/pages/assistance-view-page/assistance-view-page.component.ts
+++ src/app/pages/assistance-view-page/assistance-view-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { AppService } from 'src/app/app.service';
 
 @Component({
@@ -8,14 +9,37 @@ import { AppService } from 'src/app/app.service';
 })
 export class AssistanceViewPageComponent implements OnInit {
 
+
+  fecha:String='';
+  course_id:any;
   constructor(
     public appService:AppService,
+    private route:ActivatedRoute 
   ) { }
 
   ngOnInit(): void {
 
-    
+    this.course_id=this.route.snapshot.paramMap.get('id');
 
+  }
+
+  searchAssitance(event:any){
+    let fecha=event.target.value
+    console.log("Datos de la fecha: ",);
+
+    this.appService.getItem(`/api/assistance/${fecha}/${this.course_id}`).subscribe(
+      (response:any)=>{
+        if(response.valid){
+          this.appService.student_assitance=response.data
+
+          this.appService.student_assitance.map((item:any)=>{
+            item.estado=true;
+            return item
+          })
+        }
+        
+    
+      })
   }
 
 }
