4b8ed75c1194cb35337a7499e67de79834f96bde
Santiago cardona <Santiago.cardona@rocketfy.co>
2024-05-07 11:20:57 -0500

    componente table

---
+++ src/app/components/table/table.component.ts
@@ -0,0 +1,30 @@
+
+import { CommonModule } from '@angular/common';
+import { Component, OnInit, Input } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+
+  standalone: true,
+  selector: 'app-table',
+  templateUrl: './table.component.html',
+  styleUrls: ['./table.component.css'],
+  imports:[CommonModule, RouterModule],
+})
+export class TableComponent implements OnInit {
+  @Input() data: any[] = [];
+  @Input() titles: string[] = [];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  getObjectKeys(obj: any): string[] {
+    const newObject = {...obj}; // se utiliza desestructuracion para romper el enlace con el objeto origial
+    delete newObject._id;
+    return Object.keys(newObject);
+  }
+
+
+}
--- src/app/dashboard/teacher/teacher-list/teacher-list.component.ts
+++ src/app/dashboard/teacher/teacher-list/teacher-list.component.ts
@@ -2,18 +2,32 @@ import { Component } from '@angular/core';
 import { TeacherService } from '../teacher.service';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
+import { TableComponent } from 'src/app/components/table/table.component';
 
 @Component({
   selector: 'app-teacher-list',
   standalone: true,
-  imports: [CommonModule,RouterLink],
+  imports: [CommonModule,RouterLink, TableComponent],
   templateUrl: './teacher-list.component.html',
   styleUrl: './teacher-list.component.css'
 })
 export class TeacherListComponent {
 
+  titles = ["dni","name","phone","email"]
+  data: any[] = []
   constructor(
     public teacherService:TeacherService,
-  ){}
+  ){
+    // formatear objeto para darle la estructura
+    this.data = this.teacherService.teachers.map((teacher)=>{
+      return {
+        dni: teacher.dni,
+        name: teacher.name,
+        phone: teacher.phone,
+        email: teacher.email,
+        _id: teacher._id
+      };
+    });
+  }
 
 }
