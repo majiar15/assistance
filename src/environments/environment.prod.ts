1de91fd230cb5760106765cc8768082bb894bff3
Daniel Salinas <danielsalinassalinas09@gmail.com>
2024-11-20 20:57:03 -0500

    actualizando variable produccion

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  domain:'https://asistance-qr-6bc6a10b5951.herokuapp.com/api',
+  domain:'https://asistencia-unilibre-bcf5a90c9c34.herokuapp.com/api',
 
-  urlBase:'https://asistance-qr-6bc6a10b5951.herokuapp.com',
+  urlBase:'hhttps://asistencia-unilibre-bcf5a90c9c34.herokuapp.com',
   urlBaseMocks:'https://7ffbe5d4-9719-42c4-8711-543699a7a94d.mock.pstmn.io/',
 };
