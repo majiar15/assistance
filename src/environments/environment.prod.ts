8971fbcbdc5b86750def4e6de022e61a562287b2
Martin jimenez <mjimenez.externos@gpschile.com>
2025-02-05 08:57:41 -0500

    cambio de env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  domain:'https://asistencia-unilibre-bcf5a90c9c34.herokuapp.com/api',
+  domain:'https://asistencia-unilibre-qr-c712d878aa1c.herokuapp.com/api',
 
-  urlBase:'hhttps://asistencia-unilibre-bcf5a90c9c34.herokuapp.com',
+  urlBase:'https://asistencia-unilibre-qr-c712d878aa1c.herokuapp.com',
   urlBaseMocks:'https://7ffbe5d4-9719-42c4-8711-543699a7a94d.mock.pstmn.io/',
 };
